8b182234deae6e9e7f3ef2d698a7677b52d26919
Aditya Thakur <Aditya Thakur@Aditya>
2024-03-13 22:42:21 +0530

    git first commit

---
+++ blogapp_blogapp/src/main/java/com/myblog/blogapp/entity/POST.java
@@ -0,0 +1,78 @@
+package com.myblog.blogapp.entity;
+
+import javax.annotation.processing.Generated;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data         // lombok@ (getter or setter) we can also use @getter and @setter instead of this use @data
+@NoArgsConstructor // constructor no args 
+@AllArgsConstructor // constructor all args
+@Entity
+@Table(
+				
+		name = "posts" , 
+		uniqueConstraints = {@UniqueConstraint(columnNames = {"tittle"})}
+		
+		)
+public class POST {
+	
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private Long id;
+	
+	@Column(name = "tittle" , nullable = false )
+	private String tittle;
+	
+	@Column(name = "discription" , nullable = false)
+	private String description;
+	
+	@Column(name = "content" , nullable = false)
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTittle() {
+		return tittle;
+	}
+
+	public void setTittle(String tittle) {
+		this.tittle = tittle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+	
+
+}
